27c5b2fbde43a7dcd1cd680ff4230ef8932a2772
ikhakoo <ikhakoo@gmail.com>
2015-04-16 14:50:49 -0400

    can save videos now

--- app/controllers/tubes_controller.rb
+++ app/controllers/tubes_controller.rb
@@ -3,5 +3,10 @@ class TubesController < ApplicationController
 		@tubes = Tube.all
 	end
 
-	
+	def new
+  end
+
+  def create
+    render :text => "Saving a video. URL: #{params[:url]}, Title: #{params[:title]}, Source: #{params[:source]}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get 'tubes' => 'tubes#index'
+  
+  post 'tubes' => 'tubes#create' # this is a new line of code
+  get 'tubes/new' => 'tubes#new' # this is also a new line of code
 end
