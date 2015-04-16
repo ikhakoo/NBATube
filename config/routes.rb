f61bef86848433921905c316ecc4b1a4dd8e9773
ikhakoo <ikhakoo@gmail.com>
2015-04-16 15:52:08 -0400

    fixed routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
   root 'tubes#index'
   resources :tubes
-
-  # get 'tubes' => 'tubes#index'
-  
-  # post 'tubes' => 'tubes#create' 
-  # get 'tubes/new' => 'tubes#new' 
-
-  # get 'tubes/:id/edit' => "tubes#edit", as: "edit_tube"
-  # patch 'tubes/:id' => "tubes#update"
-
-  # delete 'tubes/:id' => 'tubes#destroy', as: "delete_tube"
-
   
 end
