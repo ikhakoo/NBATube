37596e086ac759cd69ab0979dd381b16bb6e4d67
ikhakoo <ikhakoo@gmail.com>
2015-04-16 15:03:09 -0400

    can add videos now

--- app/controllers/tubes_controller.rb
+++ app/controllers/tubes_controller.rb
@@ -4,9 +4,23 @@ class TubesController < ApplicationController
 	end
 
 	def new
+    @tube = Tube.new
   end
 
   def create
-    render :text => "Saving a video. URL: #{params[:url]}, Title: #{params[:title]}, Source: #{params[:source]}"
+    # make a new tube with what tube_params returns (which is a method we're calling)
+    @tube = Tube.new(tube_params)
+    if @tube.save
+      # if the save for the tube was successful, go to index.html.erb
+      redirect_to tubes_url
+    else
+      # otherwise render the view associated with the action :new (i.e. new.html.erb)
+      render :new
+    end
+  end
+
+  private
+  def tube_params
+    params.require(:tube).permit(:source, :title, :url)
   end
 end
